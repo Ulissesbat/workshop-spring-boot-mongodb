c89025081c6bbdaed14a2e556176acc44ca971a9
Ulisses Batista Santana <santana.gru@gmail.com>
2024-03-11 15:57:06 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/ulissessantana/workshopmongo/services/UserServices.java
+++ src/main/java/com/ulissessantana/workshopmongo/services/UserServices.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ulissessantana.workshopmongo.domain.User;
-import com.ulissessantana.workshopmongo.repository.UserRespository;
+import com.ulissessantana.workshopmongo.repository.UserRepository;
 
 @Service
 public class UserServices {
 
 	@Autowired
-	private UserRespository repo;
+	private UserRepository repo;
 	
 	public List<User> findAll(){
 		return repo.findAll();		
